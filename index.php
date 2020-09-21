82a4b1769b58d657222428a13fbd394a1c0dcdf8
Lovasz Akos <akos4x44@gmail.com>
2020-09-21 18:59:34 +0200

    added onclick call to tiles

--- index.php
+++ index.php
@@ -8,28 +8,28 @@
 
 <body>
 	<div class="wrapper">
-	  <div class="box 1" id=b1>1</div>
-	  <div class="box 2" id=b2 onclick="myFunction()">2</div>
-	  <div class="box 3" id=b3>3</div>
-	  <div class="box 4" id=b4>4</div>
-	  <div class="box 5" id=b5>5</div>
-	  <div class="box 6" id=b6>6</div>
-	  <div class="box 7" id=b7>7</div>
-	  <div class="box 8" id=b8>8</div>
-	  <div class="box 9" id=b9>9</div>
-	  <div class="box 10" id=b10>10</div>
-	  <div class="box 11" id=b11>11</div>
-	  <div class="box 12" id=b12>12</div>
-	  <div class="box 13" id=b13>13</div>
-	  <div class="box 14" id=b14>14</div>
-	  <div class="box 15" id=b15>15</div>
-	  <div class="box 16" id=b16>16</div>
+	  <div class="box 1" id=b1 onclick="flip()">1</div>
+	  <div class="box 2" id=b2 onclick="flip()">2</div>
+	  <div class="box 3" id=b3 onclick="flip()">3</div>
+	  <div class="box 4" id=b4 onclick="flip()">4</div>
+	  <div class="box 5" id=b5 onclick="flip()">5</div>
+	  <div class="box 6" id=b6 onclick="flip()">6</div>
+	  <div class="box 7" id=b7 onclick="flip()">7</div>
+	  <div class="box 8" id=b8 onclick="flip()">8</div>
+	  <div class="box 9" id=b9 onclick="flip()">9</div>
+	  <div class="box 10" id=b10 onclick="flip()">10</div>
+	  <div class="box 11" id=b11 onclick="flip()">11</div>
+	  <div class="box 12" id=b12 onclick="flip()">12</div>
+	  <div class="box 13" id=b13 onclick="flip()">13</div>
+	  <div class="box 14" id=b14 onclick="flip()">14</div>
+	  <div class="box 15" id=b15 onclick="flip()">15</div>
+	  <div class="box 16" id=b16 onclick="flip()">16</div>
 	</div>
 </body>
 </html>
 
 <script>
-function myFunction() {
+function flip() {
 	document.getElementById('b2').innerHTML = "xd";
 }
 </script>
