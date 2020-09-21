99aee10cbcc0fd604eff8e641d4a6ba9d8cf202f
Lovasz Akos <akos4x44@gmail.com>
2020-09-21 18:57:26 +0200

    added ids to tiles

--- index.php
+++ index.php
@@ -6,32 +6,30 @@
 	<title>AFP1</title>
 </head>
 
-<?php 
-	echo "hi";
-	function flip() {
-		log("clicked box2");
-	 }
-?>
-
 <body>
 	<div class="wrapper">
-	  <div class="box 1">1</div>
-	  <div onclick="flip()" class="box 2">2</div>
-	  <div class="box 3">3</div>
-	  <div class="box 4">4</div>
-	  <div class="box 5">5</div>
-	  <div class="box 6">6</div>
-	  <div class="box 7">7</div>
-	  <div class="box 8">8</div>
-	  <div class="box 9">9</div>
-	  <div class="box 10">10</div>
-	  <div class="box 11">11</div>
-	  <div class="box 12">12</div>
-	  <div class="box 13">13</div>
-	  <div class="box 14">14</div>
-	  <div class="box 15">15</div>
-	  <div class="box 16">16</div>
+	  <div class="box 1" id=b1>1</div>
+	  <div class="box 2" id=b2 onclick="myFunction()">2</div>
+	  <div class="box 3" id=b3>3</div>
+	  <div class="box 4" id=b4>4</div>
+	  <div class="box 5" id=b5>5</div>
+	  <div class="box 6" id=b6>6</div>
+	  <div class="box 7" id=b7>7</div>
+	  <div class="box 8" id=b8>8</div>
+	  <div class="box 9" id=b9>9</div>
+	  <div class="box 10" id=b10>10</div>
+	  <div class="box 11" id=b11>11</div>
+	  <div class="box 12" id=b12>12</div>
+	  <div class="box 13" id=b13>13</div>
+	  <div class="box 14" id=b14>14</div>
+	  <div class="box 15" id=b15>15</div>
+	  <div class="box 16" id=b16>16</div>
 	</div>
 </body>
 </html>
 
+<script>
+function myFunction() {
+	document.getElementById('b2').innerHTML = "xd";
+}
+</script>
