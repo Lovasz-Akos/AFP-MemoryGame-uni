67df57bac444414aba934efb242b4aa3a651df24
Lovasz Akos <akos4x44@gmail.com>
2020-09-21 19:33:17 +0200

    added trackers to tiles clicked

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-function flip() {
-    console.log("hi");
+var flippedCounter = 0;
+var newTileID = "";
+var firstTileID = "";
+
+var pictures = [""]; //contains the list of assignable pictures 
+
+function flip(tileID) { //onclick of tiles calls this function, with it's own id (for example: b4), that's stored in tileID
+    flippedCounter++;
+    newTileID = tileID; //got tileID gets stored in newTileID. This is needed to check it against the previous tile.
+
+    if (flippedCounter == 1) {
+        firstTileID = newTileID; //if this is the first tile clicked, store it as the one to compare to
+    }
+
+    if (flippedCounter == 2) {
+        //#TODO add checker logic. This needs the pictures and the randomiser to be done. 
+
+        flippedCounter = 0;
+    }
 }
