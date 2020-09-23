ffa6aa22de20efa9cf82f2c9cfaeee6b29e46337
Krigovszki Bálint <krigovszki.balint@gmail.com>
2020-09-23 03:14:11 +0200

    Implemented flipping for all tiles

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ var firstTileID = "";
 var pictures = [""]; //contains the list of assignable pictures 
 
 function flip(tileID) { //onclick of tiles calls this function, with it's own id (for example: b4), that's stored in tileID
-    showimg();
+    
     flippedCounter++;
     newTileID = tileID; //got tileID gets stored in newTileID. This is needed to check it against the previous tile.
 
     if (flippedCounter == 1) {
+        showimg(tileID);
         firstTileID = newTileID; //if this is the first tile clicked, store it as the one to compare to
 
         console.log("first click");
@@ -18,11 +19,13 @@ function flip(tileID) { //onclick of tiles calls this function, with it's own id
     }
 
     if (flippedCounter == 2) {
+        showimg(tileID);
         //TODO add checker logic. This needs the pictures and the randomiser to be done. 
         console.log("second click");
         flippedCounter = 0;
 
         if (newTileID == firstTileID) { //if the first and second click ids match, seemingly nothing happens, but I do a log just to be sure
+            hideimg(tileID);
             console.log("clicked the same tile twice. this...does nothing.");
             flippedCounter = 1; //! this should work but it needs some testing 
         }
@@ -30,9 +33,9 @@ function flip(tileID) { //onclick of tiles calls this function, with it's own id
         console.log("current:" + newTileID); //? I think this was for debugging but I'll leave them in here
     }
 }
-function showimg() {
-    document.getElementById("b1").style.backgroundImage = "url('images/fish.png')";
+function showimg(tileID) {
+    document.getElementById(tileID).style.backgroundImage = "url('images/fish.png')";
 }
-function hideimg() {
-    document.getElementById("b1").style.backgroundColor = "#443737";
+function hideimg(tileID) {
+    document.getElementById(tileID).style.backgroundColor = "#443737";
 }
