f8fc980ae6a55e7f8a987d4d6b61e40c05a0f6d5
Lovasz Akos <akos4x44@gmail.com>
2020-09-23 03:20:34 +0200

    fixed hideimg function, moved status check to external function

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var firstTileID = "";
 var pictures = [""]; //contains the list of assignable pictures 
 
 function flip(tileID) { //onclick of tiles calls this function, with it's own id (for example: b4), that's stored in tileID
-    
+
     flippedCounter++;
     newTileID = tileID; //got tileID gets stored in newTileID. This is needed to check it against the previous tile.
 
@@ -24,18 +24,25 @@ function flip(tileID) { //onclick of tiles calls this function, with it's own id
         console.log("second click");
         flippedCounter = 0;
 
-        if (newTileID == firstTileID) { //if the first and second click ids match, seemingly nothing happens, but I do a log just to be sure
-            hideimg(tileID);
-            console.log("clicked the same tile twice. this...does nothing.");
-            flippedCounter = 1; //! this should work but it needs some testing 
-        }
+        checkStatus(tileID);
         console.log("first:" + firstTileID);
         console.log("current:" + newTileID); //? I think this was for debugging but I'll leave them in here
     }
 }
+
 function showimg(tileID) {
     document.getElementById(tileID).style.backgroundImage = "url('images/fish.png')";
 }
+
 function hideimg(tileID) {
     document.getElementById(tileID).style.backgroundColor = "#443737";
+    document.getElementById(tileID).style.backgroundImage = "";
+}
+
+function checkStatus(tileID) {
+    if (newTileID == firstTileID) { //if the first and second click ids match, seemingly nothing happens, but I do a log just to be sure
+        console.log("clicked the same tile twice. this...does nothing.");
+        flippedCounter = 0; //! this should work but it needs some testing 
+        hideimg(tileID);
+    }
 }
