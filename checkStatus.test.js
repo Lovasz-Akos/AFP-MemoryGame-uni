25785fe7169438791953898c3398c21f40ce1881
Parkash9967 <49814809+Parkash9967@users.noreply.github.com>
2020-10-14 23:41:43 +0500

    test log Om

---
+++ checkStatus.test.js
@@ -0,0 +1,10 @@
+const { hasUncaughtExceptionCaptureCallback } = require('process');
+const { checkStatus } = require('./script')
+
+test ('should give true value' , () => {
+
+    const text = checkStatus('true');
+      if (newTileID == firstTileID)
+
+ expect(text).toBe('true');
+});
